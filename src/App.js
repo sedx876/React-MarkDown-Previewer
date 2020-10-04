ac8804ffc8cc0fda647ee0ad6203d573c285eb40
sedx876 <swatkins876@gmail.com>
2020-10-04 09:43:56 -0400

    added markdown text area

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ import Badge from "react-bootstrap/Badge";
 
 export default class App extends React.Component{
   render(){
+
+    var inputStyle = {
+      width: "400px",
+      height: "50vh",
+      marginLeft: "auto",
+      marginRight: "auto",
+      padding:"10px"
+    };
+
+    var outputStyle = {
+      width: "400px",
+      height: "50vh",
+      backgroundColor: "#DCDCDC",
+      marginLeft: "auto",
+      marginRight: "auto",
+      padding:"10px"
+    };
+
     return (
       <div className="App">
         <div className="container">
@@ -11,18 +29,39 @@ export default class App extends React.Component{
             <div className="col text-center">
               <h1>
                 <Badge className="text-align-center" variant="light">
-                MarkDown Previewer
+                  Markdown Previewer
                 </Badge>
               </h1>
             </div>
           </div>
           <div className="row mt-4">
             <div className="col-md-6">
-              <h2>Lorem Ipsum</h2>
+              <div className="col text-center">
+                <h4>
+                  <Badge className="text-align-center" variant="secondary">
+                    Markdown Input
+                  </Badge>
+                </h4>
+              </div>
+              <div className="mark-input" style={inputStyle}>
+                <textarea 
+                  className="input"
+                  style={inputStyle}> 
+                  </textarea>
+              </div>
             </div>
+
             <div className="col-md-6">
-              <h2>Lorem Ipsum</h2>
-            </div>
+              <div className="col text-center">
+                <h4>
+                <Badge className="text-align-center" variant="secondary">
+                  Preview
+                </Badge>
+                </h4>
+              </div>
+            <div style={outputStyle}>
+          </div>
+          </div>
           </div>
         </div>
       </div>
